e61abb0a1ee4e45cdd97032855ff2a0edbf8ca68
wheezingGekko <farrah.s.urmeneta@hotmail.ca>
2018-02-25 13:43:39 -0700

    Trying C++
    ** Part of the attempt to incorporate C++ into unreal

---
+++ AIProject2/Source/AIProject2/EnemyLeaderFunctions.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "EnemyLeaderFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT2_API UEnemyLeaderFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
