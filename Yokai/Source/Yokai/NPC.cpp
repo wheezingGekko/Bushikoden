ddff449d6c4c2fd7143b63d2c245d929d13161cb
bdragon917 <blackdragon917@gmail.com>
2018-08-27 22:18:33 -0600

    Resolving Merge2?
    Double cross fingers

--- Yokai/Source/Yokai/NPC.cpp
+++ Yokai/Source/Yokai/NPC.cpp
@@ -12,6 +12,7 @@
 ANPC::ANPC()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
 	
 	//AIControllerClass = GetClass(NewAIController)
 
